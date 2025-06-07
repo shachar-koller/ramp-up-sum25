b86d419ba4ec9d269389aeaf4616084cb195b2d3
shachar-koller <shacharyk@gmail.com>
2025-06-07 22:16:20 +0300

    moving files around

---
+++ udemy-practice-1-7/question1.py
@@ -0,0 +1,6 @@
+# 1. Compound Interest Calculation
+# Ask the user for the interest rate, principal, and number of years they are investing.
+# Output the final amount earned.
+# Use the formula:
+# final amount = principal * (1 + interest_rate) ^ years
+
