53b2ad5110a436a3289ea995326cfd769c7e13f3
shachar-koller <shacharyk@gmail.com>
2025-06-08 23:09:54 +0300

    finished question 5

--- udemy-practice-1-7/question5.py
+++ udemy-practice-1-7/question5.py
@@ -1,3 +1,14 @@
 # 5. Even Numbers Backwards
 # Use a while loop to output all the even numbers between 1 and 100, in reverse order, like this:
 # 100, 98, 96, ...
+
+i = 100
+final = []
+while True:
+    if i == 0:
+        break
+    to_append = str(i) + ", "
+    final.append(i)
+    i -= 2
+
+print(", ".join(str(num) for num in final))
