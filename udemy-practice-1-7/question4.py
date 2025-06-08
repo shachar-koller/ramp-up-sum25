79cd9d6ef7c1c4cb3f53d67e63b7258b824ee64e
shachar-koller <shacharyk@gmail.com>
2025-06-08 22:16:19 +0300

    finished question 4

--- udemy-practice-1-7/question4.py
+++ udemy-practice-1-7/question4.py
@@ -2,3 +2,11 @@
 # Use a for loop to output every number between 1 and 1000 that contains the digit 3.
 # Separate the numbers with a bar |, like this:
 # 3 | 13 | 23 | ...
+
+results = []
+for i in range (1001):
+    string_representation = str(i)
+    if "3" in string_representation:
+        results.append(str(i))
+final_output = "|".join(results)
+print(final_output)
