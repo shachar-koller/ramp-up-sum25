a02ae6ecaf3b1e193eafb411224367a550ba4d29
shachar-koller <shacharyk@gmail.com>
2025-06-08 09:39:20 +0300

    finished practice question 1

--- udemy-practice-1-7/question1.py
+++ udemy-practice-1-7/question1.py
@@ -7,6 +7,17 @@
 def main():
     print("Welcome to the Compound Interest Calculator!")
 
+    rate = float(input("What is your interest rate? "))
+    princ = float(input("what is your principal? "))
+    yrs = int(input("How many years will you be investing? "))
+
+    final = princ * (1 + rate)**yrs
+    rounded_final = round(final, 2)
+    print("\n")
+
+
+    print("Your total after compounding interest will be: $" + str(rounded_final))
+
 
 
 main()
