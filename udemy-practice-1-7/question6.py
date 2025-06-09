193ac25ada325848c8d7bb1aad293733599149ba
shachar-koller <shacharyk@gmail.com>
2025-06-09 18:04:45 +0300

    finished question 6

--- udemy-practice-1-7/question6.py
+++ udemy-practice-1-7/question6.py
@@ -3,4 +3,25 @@
 # Separate the numbers with a bar |, like this:
 # 101 | 103 | 107 | ...
 
-#i am making a change just to test github
+def finding_primes(lower_bound, upper_bound):
+    all_nums = 2
+    prime = True
+    results = []
+
+    for num_to_check in range (lower_bound, upper_bound):
+        prime = True
+        all_nums = 2
+        while all_nums < num_to_check:
+            if num_to_check % all_nums == 0:
+                prime = False
+                break
+            all_nums += 1
+        if prime:
+            results.append(num_to_check)
+    
+    return("| ".join(str(num) for num in results))
+
+def main():
+    print(finding_primes(100, 1000))
+
+main()
